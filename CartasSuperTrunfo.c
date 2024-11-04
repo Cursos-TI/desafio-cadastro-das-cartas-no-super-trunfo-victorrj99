522ac3d7099034c97fdc1c68a84984725a81e29d
João Victor <81594098+victorrj99@users.noreply.github.com>
2024-11-04 19:01:32 +0000

    Alteração Avançado

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -17,37 +17,148 @@ int main() {
     // Sugestão: Utilize a função printf para exibir as informações das cartas cadastradas de forma clara e organizada.
     // Exiba os valores inseridos para cada atributo da cidade, um por linha.
 
-    int cityCode, spottourist;
-    char cityName[50];
-    double population;
-    float area, pib;
+    #include <stdio.h>
 
-    printf("Digite o código da cidade:\n");
-    scanf("%d", &cityCode);
+float calculateAvarage(float value_1, float value_2);
+float calculatepower(float value_1, float value_2);
+float calculateDensity(float value_1, float value_2);
+int calculatePoint(float value_1, float value_2);
 
-    printf("Digite o nome da cidade:\n");
-    getchar();
+
+int main(){
+
+    // Variareis de trabalho
+    
+    int spottourist, spottourist_2;
+    char cityName[50], charterCode[10], charterState[10], cityName_2[50], charterCode_2[10], charterState_2[10];
+    float area, pib, densityPop, pibpercapita, population, area_2, pib_2, densityPop_2, pibpercapita_2, population_2,
+    areaResult, pibResult, densityPopResult, pibpercapitaResult, populationResult, spottouristResult;
+    
+
+
+    //
+
+    //Coletar dados das cartes
+
+    
+    printf("Digite o estado da primeira carta:");
+    fgets(charterState, 10, stdin);
+    
+    printf("\nDigite o código da primeira carta:");
+    fgets(charterCode, 10, stdin);
+
+
+    printf("\nDigite o nome da primeira cidade:");
     fgets(cityName, 50, stdin);
 
-    printf("Digita a população da cidade.\n");
-    scanf("%d", &population);
+    printf("\nDigita a população da primeira cidade.");
+    scanf("%f", &population);
+    getchar();
 
-    printf("Digite o PIB da cidade:\n");
+    printf("\nDigite o PIB da primeira cidade:");
     scanf("%f", &pib);
+    getchar();
 
-    printf("Quantos pontos turisticos tem a cidade?\n");
+    printf("\nQuantos pontos turisticos tem a primeira cidade?");
     scanf("%d", &spottourist);
+    getchar();
 
-    printf("Digite a área da cidade:\n");
+    printf("\nDigite a área da primeira cidade:");
     scanf("%f", &area);
+    getchar();
+
+    printf("\nDigite o estado da segunda carta: ");
+    fgets(charterState_2, 10, stdin);
+    
+    printf("\nDigite o código da segunda carta:");
+    fgets(charterCode_2, 10, stdin);
 
 
-    printf("\nCidade: %s", cityName);
-    printf("Código: %d\n", cityCode);
-    printf("Pontos turisticos: %d\n", spottourist);
-    printf("População: %d\n", population);
-    printf("Area: %.2f\n", area);
-    printf("Pib: %.2f %\n", pib);
+    printf("\nDigite o nome da segunda cidade:");
+    fgets(cityName_2, 50, stdin);
+
+    printf("\nDigita a população da segunda cidade.");
+    scanf("%f", &population_2);
+    getchar();
+
+    printf("\nDigite o PIB da segunda cidade:");
+    scanf("%f", &pib_2);
+    getchar();
+
+    printf("\nQuantos pontos turisticos tem a segunda cidade?");
+    scanf("%d", &spottourist_2);
+    getchar();
+
+    printf("\nDigite a área da segunda cidade:");
+    scanf("%f", &area_2);
+    getchar();
+
+    
+
+    //
+
+    // Calculos
+
+    densityPop = calculateAvarage(population, area);
+    pibpercapita = calculateAvarage(pib, population);
+    densityPop_2 = calculateAvarage(population_2, area_2);
+    pibpercapita_2 = calculateAvarage(pib_2, population_2);
+
+    areaResult = calculatepower(area, area_2); // Maior área
+    pibResult = calculatepower(pib, pib_2); // Maior PIB
+    densityPopResult = calculateDensity(densityPop, densityPop_2); // Menor densidade populacional
+    pibpercapitaResult = calculatepower(pibpercapita, pibpercapita_2); // Maior PIB per capita
+    populationResult = calculatepower(population, population_2); // Maior população
+    spottouristResult = calculatePoint(spottourist, spottourist_2); // Maior pontos turisticos
+
+
+
+    //
+
+    // Mostrar dados na tela do usuário.
+
+    printf("Estados duelando: Carta 1: %s x Carta 2: %s", cityName, cityName_2);
+    printf("\nCódigos das cartes em duelo: Carta 1: %s x Carta 2: %s", charterCode, charterCode_2);
+    printf("\nPopulação das cidades: Carta 1: %.0f x Carta 2: %.0f, Vencedor: %f ", population, population_2, populationResult );
+    printf("\nÁrea das cidades: Carta 1: %.2f x Carta 2: %.2f, Vencedor: %.2f ", area, area_2, areaResult);
+    printf("\nDensidade populacional: Carta 1: %.2f x Carta 2: %.2f, Vencedor: %.2f ", densityPop, densityPop_2, densityPopResult);
+    printf("\nPib: Carta 1: %.2f x Carta 2: %.2f, Vencedor: %.2f ", pib, pib_2, pibResult);
+    printf("\nPib per Capita: Carta 1: %.0f x Carta 2: %.0f, Vencedor: %.0f ", pibpercapita, pibpercapita_2, pibpercapitaResult);
+    printf("\nPontos turisticos: Carta 1: %d x Carta 2: %d, Vencedor: %d ", spottourist, spottourist_2, spottouristResult);
+
+
+    //
+
+}
+
+float calculateAvarage(float value_1, float value_2){
+    float media = value_1 / value_2;
+    return media;    
+}
+
+float calculatepower(float value_1, float value_2){
+    if (value_1 > value_2){
+        return value_1;
+    } else {
+        return value_2;
+    }
+}
+
+float calculateDensity(float value_1, float value_2){
+    if (value_1 < value_2){
+        return value_1;
+    } else {
+        return value_2;
+    }
+}
+
+int calculatePoint(float value_1, float value_2){
+    if (value_1 > value_2){
+        return value_1;
+    } else {
+        return value_2;
+    }
+}
 
     return 0;
 }
